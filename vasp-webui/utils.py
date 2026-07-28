bd1f2b535fc316522ba42c56f5675d681e5cb61b
phatdatnguyen <nguyenphatdat43@gmail.com>
2026-07-28 14:48:45 +0700

    clean repo

--- vasp-webui/utils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def get_files_in_working_directory(working_directory_path):
-    files = [f for f in os.listdir(working_directory_path) if not f.endswith('Zone.Identifier')]
-    return files
-
